bf625a101d9a4125a6b0627b8e58d394646f01ae
Minifyre <minifyre@gmail.com>
2018-09-10 14:00:54 -0500

    cleaned up
    Made imports come from same directory (and not a parent folder).
    
    Removed dead code (addChars())
    
    Simplified findReplace().

--- code.logic.mjs
+++ code.logic.mjs
@@ -1,21 +1,7 @@
 import config from './code.config.mjs'
-import tokenize from '../node_modules/tokenize/tokenize.mjs'
+import tokenize from './node_modules/tokenize/tokenize.mjs'
 const logic={}
-logic.addChars=function(rows,token,i)//@finish
-{
-    const
-    {type,start,stop}=token,
-    types=token.type.split(),
-    col=rows[rows.length-1]
-    col.push({type,text:start})
-    if (types.indexOf('newline')>-1) rows.push([])
-    return rows
-}
-logic.findReplace=function(txt,findReplaceArr)
-{
-    const [find,replace]=findReplaceArr
-    return txt.replace(new RegExp(find,'g'),replace)
-}
+logic.findReplace=(txt,[find,swap])=>txt.replace(new RegExp(find,'g'),swap)
 logic.token2color=({type},colors)=>colors[type.split(' ').find(x=>colors[x])]
 logic.tokenize=tokenize
 export {config,logic}
