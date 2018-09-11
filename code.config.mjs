52835d88abc449ff78e122825b49dde4533f25ac
Minifyre <minifyre@gmail.com>
2018-09-11 13:37:47 -0500

    +comments for future features

--- code.config.mjs
+++ code.config.mjs
@@ -6,11 +6,13 @@ const config=
 config.themes.pane=
 {
 	//html
+	//@todo make parent tokens overwrite default text color (e.g. comment text should be green)
 	text:'#fff',
 	attribute:'#90c',
 	comment:'#0c0',
 	tag:'#09c',
 	operator:'#999',
-	value:'#0cf'
+	value:'#0cf',
+	entity:'#999'//@todo why is this not working
 }
 export default config
