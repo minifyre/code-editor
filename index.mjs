743290a4cf69258999e335f51414ba8ca322b788
Minifyre <minifyre@gmail.com>
2018-10-21 12:21:23 -0500

    added/integrated util.loadPrism()
    util.loadPrism() will serve as a wrapper for importing Prism since it does not support es6 module loading out of the box & will also extend it to have a list of all available languages & make them easier to load at will.

--- index.mjs
+++ index.mjs
@@ -4,16 +4,7 @@ const
 {truth,v}=util
 export default async function code(url='/node_modules/code-editor/')
 {
-	//@todo use es6 module loading for Prism.js
-	const {err}=await util.loadScript(url+'node_modules/prism/prism.js')
-	if (err) return console.error(err)
-	util.Prism=Prism
-
-	//get available langs, themes, & plugins
-	await fetch(url+'node_modules/prism/components.js')
-	.then(res=>res.text())
-	.then(body=>new Function('components',body+'return components')())
-	.then(({languages:langs,plugins,themes})=>Object.assign(util,{langs,plugins,themes}))
+	util.Prism=await util.loadPrism(url)
 
 	await util.mkCustomEl(url,'code-editor',code.editor)
 }
--- util.mjs
+++ util.mjs
@@ -16,4 +16,39 @@ util.loadScript=function(src)//for old timey scripts that pollute global scope
 		))
 	})
 }
+
+util.loadPrism=async function(url)
+{
+	//@todo use es6 module loading for Prism.js
+	const {err}=await util.loadScript(url+'node_modules/prism/prism.js')
+	if (err) return console.error(err)
+
+	//get available langs, themes, & plugins
+	// await fetch(url+'node_modules/prism/components.js')
+	// .then(res=>res.text())
+	// .then(body=>new Function('components',body+'return components')())
+	// .then(({languages:langs,plugins,themes})=>Object.assign(util,{langs,plugins,themes}))
+
+
+
+	// //@todo use es6 module loading for Prism.js
+	// const
+	// dir=url+'node_modules/prism/',
+	// {err}=await util.loadScript(dir+'/components/prism-core.js')
+	// if (err) return console.error(err)
+
+	// console.log(Prism)
+
+	// //get available langs, themes, & plugins
+	// // await fetch(dir+'/components.js')
+	// // .then(res=>res.text())
+	// // .then(body=>new Function('components',body+'return components')())
+	// // .then(({languages:langs,plugins,themes})=>Object.assign(util,{langs,plugins,themes}))
+
+	// //var loadComponents = require('./components/index.js')
+	// return Prism
+	return Prism
+}
+
+
 export default silo
