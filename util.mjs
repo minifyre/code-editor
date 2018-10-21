7364c2eeceeedfb67e2690a36e8b66adcc5390b7
Minifyre <minifyre@gmail.com>
2018-10-21 12:30:01 -0500

    simplified util.loadScript()

--- util.mjs
+++ util.mjs
@@ -1,35 +1,33 @@
 import silo from './config.mjs'
 const {util}=silo
 //@todo eliminate
-util.loadScript=function(src)//for old timey scripts that pollute global scope
+util.loadScript=function(src)//for old scripts that mutate the global scope
 {
-	return new Promise(function(res,rej)
+	return new Promise(function(onload,onerror)
 	{
-		document.head.appendChild(Object.assign
+		document.head.appendChild
 		(
-			document.createElement('script'),
-			{
-				onerror:err=>rej({err}),
-				onload:data=>res({data}),
-				src
-			}
-		))
+			Object.assign(document.createElement('script'),{onerror,onload,src})
+		)
 	})
+	.finally(rtn=>(rtn instanceof Error?{err:rtn}:{data:rtn}))
 }
 
 util.loadPrism=async function(url)
 {
 	//@todo use es6 module loading for Prism.js
+	//const {err}=await util.loadScript(url+'node_modules/prism/components/prism-core.js')
 	const {err}=await util.loadScript(url+'node_modules/prism/prism.js')
 	if (err) return console.error(err)
 
+
 	//get available langs, themes, & plugins
-	// await fetch(url+'node_modules/prism/components.js')
-	// .then(res=>res.text())
-	// .then(body=>new Function('components',body+'return components')())
-	// .then(({languages:langs,plugins,themes})=>Object.assign(util,{langs,plugins,themes}))
+	const {languages,plugins,themes}=await fetch(url+'node_modules/prism/components.js')
+	.then(res=>res.text())
+	.then(body=>new Function('components',body+'return components')())
 
 
+	//console.log(languages)
 
 	// //@todo use es6 module loading for Prism.js
 	// const
